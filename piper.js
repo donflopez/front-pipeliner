0a3d4aaeff8dd16465a9ea2d03085c97ab3ad823
Francisco López <donflopez@gmail.com>
2015-10-01 18:11:02 +0200

    Initial release

---
+++ piper.js
@@ -0,0 +1,44 @@
+Pipeline = function () {
+  this.cbStack= [];
+
+  this._next = function (data) {
+    this._started = true;
+    if (this.cbStack.length) {
+      var cb = this.cbStack.shift(),
+          res = cb.call(this, data, this._next.bind(this));
+
+      if (res) {
+        this.data = res;
+        this._next(res);
+      }
+    }
+  };
+
+  this.pipe = function (cb) {
+    return this.res(cb);
+  };
+
+  this.store = {
+    get: function (name, del) {
+      var ret = this._store[name];
+
+      if (del) {
+        this._store[name] = null;
+      }
+
+      return ret;
+    },
+
+    set: function (name, data) {
+      this._store[name] = data;
+    },
+
+    _store: {}
+  };
+};
+
+// Pipeline.get('./path').res(function (res) {
+//   return res.data;
+// }).pipe(function (data) {
+//   return makeantoher.thing;
+// }).return();
